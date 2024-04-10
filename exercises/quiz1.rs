47c072b8bb7181fe8e2a03a33f41eb9c52a6427e
DDreame <x814061923@qq.com>
2024-04-10 17:23:00 +0800

    update rustlings

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,13 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(x: i32) -> i32{
+    let result = if x < 41{
+        x * 2
+    }else{
+        x
+    };
+    return result;
 }
